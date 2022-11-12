7dd4f1455e47f0f8be5ef888eb6290c94868170e
akshu3011 <akshata.kumbhar@hotmail.com>
2022-11-12 10:26:44 -0800

    updated the Slider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Search} from "@material-ui/icons"
+import { Search, ShoppingCartOutlined } from "@material-ui/icons"
+import Badge from '@material-ui/core/Badge';
 
 const Container = styled.div`
     height: 60px;
 `
 
-const Wrapper =styled.div`
+const Wrapper = styled.div`
     padding: 10px 20px;
     display: flex;
     align-items: center;
@@ -33,32 +34,55 @@ const Language = styled.div`
     font-size = 14px;
     cursor: pointer;
 `
-const Logo =styled
+
 
 const Center = styled.div`
     flex: 1;
 `
+const Logo = styled.h1`
+    font-weight: bold;
+    text-align: center;
+`
 const Right = styled.div`
     flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+`
+const MenuItem = styled.div`
+    font-size = 14px;
+    cursor: pointer;
+    margin-left: 25px;
+
 `
 
 const Navbar = () => {
-  return (
-    <Container>
-        <Wrapper>
-            <Left>
-                <Language>EN</Language>
-                <SearchContainer>
-                    <Input></Input>
-                    <Search></Search>
-                </SearchContainer>
-            </Left>
-            <Center>center</Center>
-            <Right>right</Right>
-        </Wrapper>
-        
-    </Container>
-  )
+    return (
+        <Container>
+            <Wrapper>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        <Input/>
+                        <Search style={{color:"gray", fontSize: 16}}/>
+                    </SearchContainer>
+                </Left>
+                <Center>
+                    <Logo>LAMA.</Logo>
+                </Center>
+                <Right>
+                    <MenuItem>REGISTER</MenuItem>
+                    <MenuItem>SIGN IN</MenuItem>
+                    <MenuItem>
+                        <Badge badgeContent={4} color="primary">
+                            <ShoppingCartOutlined />
+                        </Badge>
+                    </MenuItem>
+                </Right>
+            </Wrapper>
+
+        </Container>
+    )
 }
 
 export default Navbar
