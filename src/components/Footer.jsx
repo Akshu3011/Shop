4ca0bf2a86cec828d334b9e8f1cb45c1d637d0fe
akshu3011 <akshata.kumbhar@hotmail.com>
2022-11-13 15:46:00 -0800

    updated the code for the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,128 @@
+import { Facebook, Instagram, MailOutline, Phone, Pinterest, Room, Twitter } from "@material-ui/icons"
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+`
+
+const Left = styled.div`
+    flex: 1;
+    display: flex;
+    padding: 20px;
+    flex-direction: column;
+`
+
+const Logo = styled.h1`
+
+`
+const Description = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: #${props => props.color};
+    display: flex;
+    justify-content: center;
+    margin-right: 20px;
+    align-items: center;
+
+`
+
+
+
+
+const Center = styled.div`
+    flex: 1;
+    padding: 20px;
+`
+
+const Title = styled.h3`
+    margin-bottom: 30px;
+`
+const List = styled.ul`
+    margin: 0;
+    padding:0;
+    list-style: none;
+    display: flex;
+    flex-wrap: wrap;
+`
+
+const ListItem = styled.li`
+    width: 50%;
+    margin-bottom: 10;
+`
+
+const Right = styled.div`
+     flex: 1;
+     padding: 20px;
+`
+
+const ContactItem = styled.div`
+    margin-bottom: 20px;
+    display: flex;
+    align-items: center;
+`
+
+const Payment = styled.img`
+    
+
+`
+
+const Footer = () => {
+    return (
+        <Container>
+            <Left>
+                <Logo>LAMA.</Logo>
+                <Description>
+                    What you wear is how you present yourself to the world, especially today, when human contacts are so quick. Fashion is instant language.
+                </Description>
+                <SocialContainer>
+                    <SocialIcon color="3B5999">
+                        <Facebook />
+                    </SocialIcon>
+                    <SocialIcon color="E4405F">
+                        <Instagram />
+                    </SocialIcon>
+                    <SocialIcon color="55ACEE">
+                        <Twitter />
+                    </SocialIcon>
+                    <SocialIcon color="E60023">
+                        <Pinterest />
+                    </SocialIcon>
+
+                </SocialContainer>
+            </Left>
+            <Center>
+                <Title>Useful Links</Title>
+                <List>
+                    <ListItem>Home</ListItem>
+                    <ListItem>Cart</ListItem>
+                    <ListItem>Men Fashion</ListItem>
+                    <ListItem>Women Fashion</ListItem>
+                    <ListItem>Accessories</ListItem>
+                    <ListItem>My Account</ListItem>
+                    <ListItem>Order Tracking</ListItem>
+                    <ListItem>Wishlist</ListItem>
+                    <ListItem>Terms</ListItem>
+                    <ListItem>Terms</ListItem>
+                </List>
+            </Center>
+            <Right>
+                <Title>Contact</Title>
+                <ContactItem><Room />622 Dixie Path, South Tobinchester 98336</ContactItem>
+                <ContactItem> <Phone />+1 234 456 678</ContactItem>
+                <ContactItem><MailOutline /> contact@lama.com</ContactItem>
+                <Payment src="https://res.cloudinary.com/dpms34wpa/image/upload/c_scale,w_292/v1668382390/Shop_App/payment-methods_ag6bwn.png" />
+
+            </Right>
+        </Container>
+    )
+}
+
+export default Footer
