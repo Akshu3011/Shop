8b68247d856ec21d01eef473a2b271d50c4a6e42
akshu3011 <akshata.kumbhar@hotmail.com>
2022-11-13 02:32:29 -0800

    updated the styled components for products

---
+++ src/components/Newsletter.jsx
@@ -0,0 +1,25 @@
+
+
+const Container =styled.div``
+const Title =styled.h1``
+const Desc =styled.div``
+const InputContainer =styled.div``
+const Input =styled.input``
+const Button =styled.button``
+
+const Newsletter = () => {
+  return (
+    <Container>
+        <Title></Title>
+        <Description></Description>
+        <InputContainer>
+            <Input/>
+            <Button>
+                <Send/>
+            </Button>
+        </InputContainer>
+    </Container>
+  )
+}
+
+export default Newsletter
