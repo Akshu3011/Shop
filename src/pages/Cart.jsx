6e35220a956f164ca32cd1fa20cba7da167ad7f8
akshu3011 <akshata.kumbhar@hotmail.com>
2022-11-13 21:20:56 -0800

    updated the summary in cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -106,12 +106,32 @@ const Hr = styled.hr`
 `
 const Summary=styled.div`
     flex: 1;
+    border: 0.5px solid lightgray;
+    border-radius: 10px;
+    padding: 20px;
+    height: 50vh;
 `
-const SummaryTitle=styled.h1``
-const SummaryItem=styled.div``
+const SummaryTitle=styled.h1`
+    font-weight: 200;
+`
+
+const SummaryItem=styled.div`
+    margin: 30px 0px;
+    display: flex;
+    justify-content: space-between;
+    font-weight: ${props => props.type === "total" && "500"};
+    font-size: ${props => props.type === "total" && "24px"};
+`
 const SummaryItemText=styled.span``
 const SummaryItemPrice=styled.span``
-const SummaryButton=styled.button``
+
+const Button=styled.button`
+    width:100%;
+    padding: 10px;
+    background-color: black;
+    color: white;
+    font-weight: 600;
+`
 
 const Cart = () => {
   return (
@@ -184,8 +204,8 @@ const Cart = () => {
                         <SummaryItemText>Shipping Discount</SummaryItemText>
                         <SummaryItemPrice>$ -5.90</SummaryItemPrice>
                     </SummaryItem>
-                    <SummaryItem>
-                        <SummaryItemText type="total">Subtotal</SummaryItemText>
+                    <SummaryItem type="total">
+                        <SummaryItemText >Subtotal</SummaryItemText>
                         <SummaryItemPrice>$ 50</SummaryItemPrice>
                     </SummaryItem>
                     <Button>CHECKOUT NOW</Button>
