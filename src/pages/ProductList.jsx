92a6c19fbb9960bc93cd63708cbcd748e7e706a4
akshu3011 <akshata.kumbhar@hotmail.com>
2022-11-13 17:17:03 -0800

    added the filter component

---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components"
+import Announcement from "../components/Announcement"
+import Footer from "../components/Footer"
+import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
+import Products from "../components/Products"
+
+const Container =styled.div`
+`
+
+const Title =styled.h1`
+    margin: 20px;
+`
+
+const FilterContainer =styled.div`
+    display: flex;
+    justify-content: space-between;
+
+
+`
+
+const Filter =styled.div`
+    margin: 20px;
+`
+
+
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar/>
+        <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+            <Filter>filer</Filter>
+            <Filter>filter1</Filter>
+        </FilterContainer>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
+    </Container>
+  )
+}
+
+export default ProductList
